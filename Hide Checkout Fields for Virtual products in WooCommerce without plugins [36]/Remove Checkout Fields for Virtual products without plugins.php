cca5b58c1c074b2bd619748e35413aa034a9a9ab
maya <maya@core>
2026-07-27 11:55:03 +0330

    36

---
+++ Hide Checkout Fields for Virtual products in WooCommerce without plugins [36]/Remove Checkout Fields for Virtual products without plugins.php
@@ -0,0 +1,26 @@
+/**
+ * Remove Checkout Fields for Virtual products in WooCommerce
+ * @WPCookie
+ */
+ 
+add_filter( 'woocommerce_checkout_fields', 'simplify_checkout_virtual' );
+  
+function simplify_checkout_virtual( $fields ) {
+   $only_virtual = true;
+   foreach( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+      // Check if there are non-virtual products
+      if ( ! $cart_item['data']->is_virtual() ) $only_virtual = false;
+   }
+   if ( $only_virtual ) {
+      unset($fields['billing']['billing_company']);
+      unset($fields['billing']['billing_address_1']);
+      unset($fields['billing']['billing_address_2']);
+      unset($fields['billing']['billing_city']);
+      unset($fields['billing']['billing_postcode']);
+      unset($fields['billing']['billing_country']);
+      unset($fields['billing']['billing_state']);
+      unset($fields['billing']['billing_phone']);
+      add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+   }
+   return $fields;
+}
